0e2902009992b980d586b022b9b24ffee45e2436
Kevin Woley <kmwoley@users.noreply.github.com>
2020-02-08 20:47:03 -0800

    moving secrets file to a temp file name to avoid conflicts

--- secrets_template.ps1
+++ secrets_template.ps1
@@ -1,3 +1,7 @@
+# Template file for backup destination configuration and email passwords.
+# Update this file to point to your restic repository and email service.
+# Rename to `secrets.ps1`
+
 # restic backup repository configuration
 $Env:AWS_ACCESS_KEY_ID='<KEY>'
 $Env:AWS_SECRET_ACCESS_KEY='<KEY>'
