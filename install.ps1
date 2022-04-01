ceedf56e3e06bc53162d76493138ad7614f46026
SLiX <SLiX69@users.noreply.github.com>
2022-04-01 18:22:57 +0200

    Update installer - restic version 0.15.0
    Update installer - restic version 0.15.0
    
    Restic version 0.15.0 was released on 12th Jan 2023
    https://restic.net/blog/2023-01-12/restic-0.15.0-released/

--- install.ps1
+++ install.ps1
@@ -5,10 +5,10 @@
 if(-not (Test-Path $ResticExe)) {
     $url = $null
     if([Environment]::Is64BitOperatingSystem){
-        $url = "https://github.com/restic/restic/releases/download/v0.12.1/restic_0.12.1_windows_amd64.zip"
+        $url = "https://github.com/restic/restic/releases/download/v0.15.0/restic_0.15.0_windows_amd64.zip"
     }
     else {
-        $url = "https://github.com/restic/restic/releases/download/v0.12.1/restic_0.12.1_windows_386.zip"
+        $url = "https://github.com/restic/restic/releases/download/v0.15.0/restic_0.15.0_windows_386.zip"
     }
     $output = Join-Path $InstallPath "restic.zip"
     Invoke-WebRequest -Uri $url -OutFile $output
