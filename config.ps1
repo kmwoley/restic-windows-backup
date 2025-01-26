33163ecb603c2a508fdf7e7ca6cfb2abedfaa74e
Kevin Woley <kmwoley@gmail.com>
2025-01-26 10:41:09 -0800

    reorganize the layout/order of the config.ps1 file

--- config.ps1
+++ config.ps1
@@ -1,31 +1,23 @@
-# backup configuration
+# general configuration
 $ExeName = "restic.exe"
-$AdditionalParameters = @()
-$SelfUpdateParameters = @()
-$AllowResticSelfUpdate = $true
 $InstallPath = "C:\restic"
 $ResticExe = Join-Path $InstallPath $ExeName
 $StateFile = Join-Path $InstallPath "state.xml"
-$WindowsExcludeFile = Join-Path $InstallPath "windows.exclude"
-$LocalExcludeFile = Join-Path $InstallPath "local.exclude"
 $LogPath = Join-Path $InstallPath "logs"
 $LogRetentionDays = 30
 $InternetTestAttempts = 10
 $GlobalRetryAttempts = 4
-$IgnoreMissingBackupSources = $false
-$AdditionalBackupParameters = @("--exclude-if-present", ".nobackup", "--no-scan")
-
-# maintenance configuration
-$SnapshotMaintenanceEnabled = $true
-$SnapshotRetentionPolicy = @("--host", $env:COMPUTERNAME, "--group-by", "host,tags", "--keep-daily", "30", "--keep-weekly", "52", "--keep-monthly", "24", "--keep-yearly", "10")
-$SnapshotPrunePolicy = @("--max-unused", "1%")
-$SnapshotMaintenanceInterval = 7
-$SnapshotMaintenanceDays = 30
-$SnapshotDeepMaintenanceDays = 90
+$AdditionalParameters = @()
 
 # email configuration
-$SendEmailOnSuccess = $false
+$SendEmailOnSuccess = $true
 $SendEmailOnError = $true
+
+# backup configuration
+$WindowsExcludeFile = Join-Path $InstallPath "windows.exclude"
+$LocalExcludeFile = Join-Path $InstallPath "local.exclude"
+$IgnoreMissingBackupSources = $false
+$AdditionalBackupParameters = @("--exclude-if-present", ".nobackup", "--no-scan")
 
 # Paths to backup
 $BackupSources = @{}
@@ -40,3 +32,15 @@ $BackupSources["C:\"] = @(
 #$BackupSources["DRIVE_LABEL_NAME_OR_SERIAL_NUMBER"] = @(
 #    "Example\FolderName"
 #)
+
+# maintenance configuration
+$SnapshotMaintenanceEnabled = $true
+$SnapshotRetentionPolicy = @("--host", $env:COMPUTERNAME, "--group-by", "host,tags", "--keep-daily", "30", "--keep-weekly", "52", "--keep-monthly", "24", "--keep-yearly", "10")
+$SnapshotPrunePolicy = @("--max-unused", "1%")
+$SnapshotMaintenanceInterval = 7
+$SnapshotMaintenanceDays = 30
+$SnapshotDeepMaintenanceDays = 90
+
+# restic.exe self update configuration
+$SelfUpdateEnabled = $true
+$SelfUpdateParameters = @()
